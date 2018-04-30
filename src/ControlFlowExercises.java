7b09578b6bad70183b547cbbd65ec087c443f380
danielconner <metalman1342@gmail.com>
2018-04-30 15:27:35 -0500

    finished the table maker part

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i<=15) {
-            System.out.println(i++ + " ");
-        }
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        Scanner sc = new Scanner(System.in);
+//        int i = 5;
+//        while (i<=15) {
+//            System.out.println(i++ + " ");
+//        }
+
 //        int x = 0;
 //        do {
 //            System.out.println(x++);
@@ -17,12 +19,68 @@ public class ControlFlowExercises {
 //            x-=5;
 //        } while (x>=-10);
 
-        long x = 2;
-        do {
-            System.out.println(x);
-            x*=x;
-        } while (x<=1000000);
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+//        long x = 2;
+//        do {
+//            System.out.println(x);
+//            x*=x;
+//        } while (x<=1000000);
+
+        for (int i = 5;i<=15;i++) {
+            System.out.println(i);
+        }
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-");
+//        for (int e = 0;e<=100;e+=2) {
+//            System.out.println(e);
+//        }
+//
+//        for (int e = 100;e>=-10;e-=5) {
+//            System.out.println(e);
+//        }
+
+        for (long e = 2;e<1000000;e*=e) {
+            System.out.println(e);
+        }
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-");
+        for (int f = 1;f<=100;f++) {
+            int trimult = f % 3;
+            int pentmult = f % 5;
+            if (trimult == 0 && pentmult == 0) {
+                System.out.println("fizzbuzz ");
+            }else if (trimult == 0) {
+                System.out.println("fizz ");
+            }else if (pentmult == 0) {
+                System.out.println("buzz ");
+            }else {
+                System.out.println(f + " ");
+            }
+        }
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-");
+        long num;
+        System.out.print("what number would you like to go up to?: ");
+        num = sc.nextInt();
+        System.out.println("Here is your Table!");
+        System.out.println();
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 0;i<=num;i++) {
+            int iter = i;
+            int isq = iter * iter;
+            int icu = isq * i;
+            if (iter >= 10) {
+                System.out.print(iter + "     | ");
+            }else {
+                System.out.print(iter + "      | ");
+            }
+            if (isq>=100) {
+                System.out.print(isq+"     | ");
+            }else if(isq>=10) {
+                System.out.print(isq+"      | ");
+            }else {
+                System.out.print(isq+"       | ");
+            }
+            System.out.println(icu);
+        }
+
 
     }
 }
