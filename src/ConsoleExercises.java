f6fdc5e2390563bcac3755d39408e8127a92a1d8
danielconner <metalman1342@gmail.com>
2018-04-30 13:34:04 -0500

    console exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int ioNum;
+//        System.out.print("Please input an int: ");
+//        ioNum = sc.nextInt();
+//        System.out.print("You input the number ");
+//        System.out.println(ioNum);
+
+//        String un;
+//        String deux;
+//        String troi;
+//        System.out.print("Please input 3 words: ");
+//        un = sc.next();
+//        deux = sc.next();
+//        troi = sc.next();
+//        System.out.println(troi +" "+ deux +" "+ un);
+
+//        String sentence;
+//        System.out.print("Please input a sentence: ");
+//        sentence = sc.nextLine();
+//        System.out.println("You input the sentence: "+sentence);
+
+        int length;
+        int width;
+        int height;
+        System.out.print("Please input room length: ");
+        length = sc.nextInt();
+        System.out.print("Please input room width: ");
+        width = sc.nextInt();
+        System.out.print("Please input height: ");
+        height = sc.nextInt();
+        System.out.println();
+        System.out.println("area: "+(length*width));
+        System.out.println("perimeter: "+((2*length)+(2*width)));
+        System.out.println("volume: "+(length*width*height));
+    }
+}
