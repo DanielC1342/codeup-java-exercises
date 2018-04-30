db42ffbfec3e093078b5a115a493751b27e216b1
danielconner <metalman1342@gmail.com>
2018-04-30 11:31:53 -0500

    syntax/types/variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,30 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
         System.out.println(" World!");
+        System.out.println();
+        int myFavoriteNumber = 1342;
+        System.out.println(myFavoriteNumber);
+        String myString = "Sphinx of black quartz, hear my vow!";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        System.out.println();
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        System.out.println();
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+        int b = 3;
+        int c = 4;
+        c *= b;
+        System.out.println(c);
+        int d = 10;
+        int e = 2;
+        d /= e;
+        e -= d;
+        System.out.println(e);
     }
 }
+//comments look like this
