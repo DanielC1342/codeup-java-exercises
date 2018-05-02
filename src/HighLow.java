bf626954a0a028ef7eedd15552d415eaa10125ce
danielconner <metalman1342@gmail.com>
2018-05-02 09:37:58 -0500

    added limited guesses

--- src/HighLow.java
+++ src/HighLow.java
@@ -7,20 +7,31 @@ public class HighLow {
     public static void game() {
         System.out.println("Im thinking of a number between 1 and 100...");
         int num = (int)(Math.random()*(100))+1;
-        guess(num);
+        guess(num,0);
     }
-    public static void guess(int num) {
+    public static void guess(int num, int guessnum) {
         Scanner sc = new Scanner(System.in);
+        if (guessnum > 9) {
+            System.out.println("GAME OVER!");
+            System.out.println("Play again? [y/n]");
+            String playagain = sc.next();
+            if (playagain.equals("y")) {
+                game();
+            }
+        }
+        System.out.println("You have "+(10-guessnum)+" guesses left");
         System.out.print("Guess: ");
         int guess = sc.nextInt();
         if (guess > num) {
-            System.out.println("GO LOWER");
-            guess(num);
+            System.out.println("TOO HIGH");
+            guessnum++;
+            guess(num,guessnum);
         }else if (guess < num) {
-            System.out.println("GO HIGHER");
-            guess(num);
+            System.out.println("TOO LOW");
+            guessnum++;
+            guess(num,guessnum);
         }else {
-            System.out.println("GOOD GUESS!");
+            System.out.println("YOU WIN!");
             System.out.println("Play again? [y/n]");
             String playagain = sc.next();
             if (playagain.equals("y")) {
