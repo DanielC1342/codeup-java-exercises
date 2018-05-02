d8a7219a1cd403f5e5692903cbe3ee112675003a
danielconner <metalman1342@gmail.com>
2018-05-02 09:15:36 -0500

    basic functionality works

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class HighLow {
     public static void main(String[] args) {
-
-        System.out.println("Im thinking of a number between 1 and 100...");
         game();
     }
     public static void game() {
+        System.out.println("Im thinking of a number between 1 and 100...");
         int num = (int)(Math.random()*(100))+1;
-
+        guess(num);
+    }
+    public static void guess(int num) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Guess: ");
+        int guess = sc.nextInt();
+        if (guess > num) {
+            System.out.println("GO LOWER");
+            guess(num);
+        }else if (guess < num) {
+            System.out.println("GO HIGHER");
+            guess(num);
+        }else {
+            System.out.println("GOOD GUESS!");
+            System.out.println("Play again? [y/n]");
+            String playagain = sc.next();
+            if (playagain.equals("y")) {
+                game();
+            }
+        }
     }
 }
