588073a0c520e1bbedebe04ebd523d7c01259f36
danielconner <metalman1342@gmail.com>
2018-05-02 12:14:48 -0500

    it work

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,13 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adj = {"Liquid","Vulcan","Sniper","Decoy","Revolver","Psycho","Laughing","Raging","Crying","Screaming","The","Big","Solid"};
+        String[] noun = {"Snake","Raven","Wolf","Octopus","Mantis","Ocelot","End","Pain","Fury","Sorrow","Fear","Boss","Joy"};
+        String code = returnRando(adj);
+        String name = returnRando(noun);
+        System.out.println("Your code name is: " + code + "-" + name);
+    }
+    public static String returnRando(String[] words) {
+        int rando = (int)(Math.random()*(words.length-1));
+        return words[rando];
+    }
+}
