707e971b7076a4e34498ef0fde92b68f6654c23b
danielconner <metalman1342@gmail.com>
2018-05-03 16:06:24 -0500

    init commit

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,17 @@
+package movies;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("0 - Exit");
+        System.out.println("1 - View all movies");
+        System.out.println("2 - View movies in the Animated Genre");
+        System.out.println("3 - View movies in the Drama Genre");
+        System.out.println("4 - View movies in the Horror Genre");
+        System.out.println("5 - View movies in the Sci-Fi Genre");
+        System.out.println();
+        System.out.print("Enter Your Selection: ");
+    }
+}
