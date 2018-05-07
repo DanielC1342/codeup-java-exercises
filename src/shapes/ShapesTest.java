307fb8f74f4086a524961a1b2ccf061e68089f3e
danielconner <metalman1342@gmail.com>
2018-05-07 09:26:37 -0500

    overrode getArea and getPerimeter in Square

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -10,6 +10,5 @@ public class ShapesTest {
         System.out.println("box2 Perimeter is "+box2.getPerimeter()); //20
         System.out.println("box2 Area is " + box2.getArea()); // 25
         System.out.println();
-
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle {
+    private int side;
+
     public Square(int sides) {
         super(sides,sides);
+        side = sides;
     }
 
+    public int getArea() {
+        System.out.println("Sq. Override");
+        return (side*side);
+    }
+    public int getPerimeter() {
+        System.out.println("Sq. Override");
+        return (side*4);
+    }
 }
