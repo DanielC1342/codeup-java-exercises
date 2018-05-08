292a23cb8a9dce2a908649f443de3de03e531204
danielconner <metalman1342@gmail.com>
2018-05-08 15:07:12 -0500

    Grade Application works fine

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,84 @@
+package grades;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        HashMap<String,Student> students = new HashMap<>();
+
+        Student tallesin = new Student("Tallesin");
+        tallesin.addGrade(90);
+        tallesin.addGrade(95);
+        tallesin.addGrade(85);
+        Student marisha = new Student("Marisha");
+        marisha.addGrade(80);
+        marisha.addGrade(85);
+        marisha.addGrade(83);
+        Student travis = new Student("Travis");
+        travis.addGrade(90);
+        travis.addGrade(91);
+        travis.addGrade(88);
+        Student sam = new Student("Sam");
+        sam.addGrade(75);
+        sam.addGrade(75);
+        sam.addGrade(70);
+        Student laura = new Student("Laura");
+        laura.addGrade(90);
+        laura.addGrade(85);
+        laura.addGrade(88);
+        Student liam = new Student("Liam");
+        liam.addGrade(90);
+        liam.addGrade(100);
+        liam.addGrade(95);
+        Student ashley = new Student("Ashley");
+        ashley.addGrade(90);
+        ashley.addGrade(92);
+        ashley.addGrade(90);
+
+        students.put("MollyMauk",tallesin);
+        students.put("BleuBeau",marisha);
+        students.put("Fjord-Tough",travis);
+        students.put("Nott_Brave",sam);
+        students.put("OskarXJester",laura);
+        students.put("Dirt_Wizard",liam);
+        students.put("TallDark&Ragey",ashley);
+
+        execute(students);
+    }
+
+    public static void execute(HashMap<String,Student> students) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here are the Students usernames: ");
+        for(HashMap.Entry<String,Student> entry: students.entrySet()) {
+            System.out.print(" |"+entry.getKey()+"| ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Which student do you want more info on? (case sensitive)");
+        System.out.print(">");
+        String response = sc.nextLine();
+        if(students.containsKey(response)) {
+            System.out.println("Name: " + students.get(response).getName() + " - GitHub Username: " + response);
+            System.out.println("Current Average: " + students.get(response).getGradeAvg());
+            System.out.println();
+        }
+        else {
+            System.out.println("Not a valid username");
+            System.out.println();
+            execute(students);
+        }
+        System.out.println();
+        System.out.println("Would you like to see another student? [y/n]");
+        System.out.print(">");
+        String repeat = sc.next();
+        if(repeat.equals("y")||repeat.equals("Y")) {
+            System.out.println();
+            execute(students);
+        }
+        else {
+            System.exit(0);
+        }
+
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -26,16 +26,4 @@ public class Student {
         }
         return total/grades.size();
     }
-
-    public static void main(String[] args) {
-        Student daniel = new Student("Daniel");
-        daniel.addGrade(95);
-        daniel.addGrade(85);
-        daniel.addGrade(90);
-        daniel.addGrade(95);
-        System.out.println("Student Name: "+daniel.getName());
-        System.out.println("Grades: "+daniel.grades.toString());
-        System.out.println("Grade Avg: "+daniel.getGradeAvg());
-
-    }
 }
